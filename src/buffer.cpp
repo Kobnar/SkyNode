4ce3071ce303de9d5ec22aa35eab58eb8ccc2a81
Kobnar <konradicus.ludwidicus.maximus@gmail.com>
2015-11-30 08:31:46 -0800

    Update docstrings for Buffer

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -4,12 +4,14 @@ using namespace std;
 using namespace cv;
 namespace skynode {
 	
+	// Primary constructor.
 	Buffer::Buffer(VideoCapture capture_source)
 	{
 		// Save the VideoSource object
 		source_ = capture_source;
 		
 		// Capture and pre-process the initial matrix
+		source_.read(raw_[cursor_]);
 		preProcess(cursor_);
 		
 		// Create an empty difference matrix
@@ -20,30 +22,32 @@ namespace skynode {
 		
 	} // Buffer::Buffer(VideoCapture capture_source)
 	
+	// Pre-processes a raw image matrix.
 	void Buffer::preProcess(int idx)
 	{
-		// Capture a new matrix
-		source_.read(raw_[idx]);
-		
-		// Blur the new matrix
+		// Blur the matrix
 		blur(
 			raw_[idx],
 			processed_[idx],
 			Size(PREPROCESS_BLUR_, PREPROCESS_BLUR_));
 		
-		// Convert the new matrix to grayscale
+		// Convert the matrix to grayscale
 		cvtColor(
 			processed_[idx],
 			processed_[idx],
 			COLOR_BGR2GRAY);
 	} // void Buffer::preProcess(int idx)
 	
+	// Cycles the buffer by calculating a new cursor, capturing a new image matrix,
+	// pre-proccessing the new matrix and calculating an absolute difference between
+	// the new matrix and the old one.
 	void Buffer::step()
 	{
 		// Calculate the new matrix' index
 		int new_cursor = (cursor_ + 1) % BUFFER_LENGTH_;
 		
-		// Capture the new matrix and process a difference map
+		// Capture and pre-process a new image matrix
+		source_.read(raw_[new_cursor]);
 		preProcess(new_cursor);
 		
 		// Calculate the absolute difference between the two frames
@@ -56,16 +60,19 @@ namespace skynode {
 		cursor_ = new_cursor;
 	} // void Buffer::step()
 	
+	// Returns the most recent raw image matrix.
 	Mat Buffer::getRaw()
 	{
 		return raw_[cursor_];
 	} // Mat Buffer::getRaw()
 	
+	// Returns the most recent pre-processed image matrix.
 	Mat Buffer::getProcessed()
 	{
 		return processed_[cursor_];
 	} // Mat Buffer::getProcessed()
 	
+	// Returns the most recent absolute difference matrix.
 	Mat Buffer::getDifference()
 	{
 		return difference_;
--- src/buffer.h
+++ src/buffer.h
@@ -1,7 +1,6 @@
 #ifndef SKYNODE_BUFFER_H_
 #define SKYNODE_BUFFER_H_
 
-#include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 
@@ -9,30 +8,36 @@ using namespace std;
 using namespace cv;
 namespace skynode {
 	
-	//	The main frame processor and buffer
-	// Designed to optimize effective memory management techniques
-	// while processing each frame from the capture source
+	// Captures and processes image matrices from a given VideoCapture object
+	// so they can be used with higher level "spot and track" logic in a
+	// parent Node.
 	class Buffer
 	{
 	private:
-		// TODO: settings from file/args
+		// Settings:
+		// 	TODO: Fetch settings from file/args
 		static const int PREPROCESS_BLUR_ = 2;
 		static const int BUFFER_LENGTH_ = 2;
 		
+		// Members:
 		VideoCapture source_;
 		Mat raw_ [BUFFER_LENGTH_];
 		Mat processed_ [BUFFER_LENGTH_];
 		Mat difference_;
 		bool cursor_ = 0;
 		
+		// Private methods:
 		void preProcess(int idx);
 		
 	public:
+		// Constructors:
 		Buffer() {};
 		Buffer(VideoCapture capture_source);
 	
+		// The main "step" method to cycle a new frame:
 		void step();
 	
+		// Interface for class members:
 		Mat getRaw();
 		Mat getProcessed();
 		Mat getDifference();
